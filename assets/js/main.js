7d25ad7a680fc5f111c60ca24c85b329995116d5
Ibrahim Rahimi <ebrahimrahimilaziri@gmail.com>
2021-09-11 14:46:39 +0430

    Complete Porfolio section design and functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,13 +94,13 @@ modalCloses.forEach((modalClose, i) => {
 /* ++++++++ PORTFOLIO SWIPER ++++++++ */
 let swiper = new Swiper(".portfolio__container", {
     cssMode: true,
+    loop:true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
 });
